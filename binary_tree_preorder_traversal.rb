0217be85eea7c63f1a478190d3dd033365e58b90
Alex <alexanderhapp@me.com>
2018-09-06 08:04:18 -0700

    Add binary tree preorder traversal

---
+++ binary_tree_preorder_traversal.rb
@@ -0,0 +1,35 @@
+# Given a binary tree, return the preorder traversal of its nodes' values.
+
+# Example:
+
+# Input: [1,null,2,3]
+#    1
+#     \
+#      2
+#     /
+#    3
+
+# Output: [1,2,3]
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer[]}
+
+
+def preorder_traversal(root, ar = [])
+     if root
+         ar << root.val
+         preorder_traversal(root.left, ar)
+         preorder_traversal(root.right, ar)
+     end 
+    ar
+end
